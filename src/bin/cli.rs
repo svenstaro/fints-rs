3cc2ed737b3bf914d52514780a693e201fad73a7
Rafael Epplee <raffomania@gmail.com>
2019-01-05 19:27:05 +0100

    use testing credentials from fints server

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -5,8 +5,8 @@ pub fn main() {
     let client = PinTanClient {
         url: "http://127.0.0.1:3000/cgi-bin/hbciservlet".to_string(),
         bank_code: 12345678,
-        username: "test".to_string(),
-        pin: "12345".to_string(),
+        username: "test1".to_string(),
+        pin: "1234".to_string(),
     };
     let accounts = client.get_accounts();
     println!("{:#?}", client);
