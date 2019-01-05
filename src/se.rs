630e65f10870078fe862343e915853c26f1c960b
Sven-Hendrik Haase <svenstaro@gmail.com>
2019-01-05 19:07:01 +0100

    Fix serialization a bit

--- src/se.rs
+++ src/se.rs
@@ -29,11 +29,30 @@ impl std::error::Error for Error {
 }
 
 pub struct Serializer {
+    /// The final serialized output string.
     output: String,
+
+    /// Keep track of whether we're currently inside a DEG or not.
+    /// This is necessary because DEGs are delmited differently than DEs.
     inside_deg: bool,
+
+    /// The current field's index in the current struct.
     field_index_in_struct: u32,
+
+    /// Total size of the last struct.
     last_struct_size: usize,
-    new_segment_starts_here: bool,
+
+    /// Stack of struct names.
+    /// We need this to be able to correctly count recursively nested structs.
+    struct_stack: Vec<String>,
+
+    /// Total number of elements in the current segment.
+    /// This is set to the size of the struct every time we pass a new segment.
+    /// This is important to know so we know when a segment ends.
+    current_segment_elements_count: usize,
+
+    /// The element position inside the current segment.
+    current_segment_index: u32,
 }
 
 pub fn to_string<T>(value: &T) -> Result<String>
@@ -45,7 +64,9 @@ where
         inside_deg: false,
         field_index_in_struct: 0,
         last_struct_size: 0,
-        new_segment_starts_here: false,
+        struct_stack: vec![],
+        current_segment_elements_count: 0,
+        current_segment_index: 0,
     };
     value.serialize(&mut serializer)?;
     Ok(serializer.output)
@@ -211,16 +232,17 @@ impl<'a> ser::Serializer for &'a mut Serializer {
     fn serialize_struct(self, name: &'static str, len: usize) -> Result<Self::SerializeStruct> {
         self.field_index_in_struct = 0;
         self.last_struct_size = len;
+        self.struct_stack.push(name.to_string());
 
-        // Keep track of when a segment starts because segments have to be terminated with a `'`.
+        // If this is a new segment, set the length so that we also know when we're finished with
+        // this particular segment.
         if name.starts_with("Seg") {
-            self.new_segment_starts_here = true;
-        } else {
-            self.new_segment_starts_here = false;
+            self.current_segment_elements_count = len;
+            self.current_segment_index = 0;
         }
 
-        // Keep track of when a DEG starts since DEs inside of a DEG are delimited using
-        // `:`. Outside of oa DEG, DEs are delimited using `+`.
+        // Keep track of when a DEG starts since DEs inside of a DEG are delimited using `:`.
+        // Outside of oa DEG, DEs are delimited using `+`.
         if name.starts_with("DEG") {
             self.inside_deg = true;
         } else {
@@ -249,8 +271,12 @@ impl<'a> ser::SerializeStruct for &'a mut Serializer {
     where
         T: ?Sized + Serialize,
     {
-        if self.new_segment_starts_here && !self.output.is_empty() {
-            self.output += "'\r\n";
+        // If the parent struct containing this field is a segment, we'll increase the current
+        // segment counter.
+        if let Some(last) = self.struct_stack.last() {
+            if last.starts_with("Seg") {
+                self.current_segment_index += 1;
+            }
         }
 
         if self.field_index_in_struct != 0 {
@@ -265,11 +291,29 @@ impl<'a> ser::SerializeStruct for &'a mut Serializer {
     }
 
     fn end(self) -> Result<()> {
+        // if self.current_segment_index == self.current_segment_elements_count as u32
+        //     && self.struct_stack.last().unwrap().starts_with("Seg")
+        // {
+        //     self.output += "|";
+        // }
+
+        // In case this is a segment, we have to terminate it with `'`.
+        if let Some(last) = self.struct_stack.last() {
+            if last.starts_with("Seg") && !self.output.is_empty() {
+                self.output += "'";
+            }
+        }
+
         // If we read the last field in this struct, we should reset the data we set for this
         // struct.
         if self.field_index_in_struct >= self.last_struct_size as u32 {
             self.inside_deg = false;
         }
+
+        // This marks the end of a parsed struct so we have to pop it from the stack at the very
+        // end.
+        self.struct_stack.pop();
+
         Ok(())
     }
 }
